bd4bf647f443c45d936b5a5d97361a95c4db3efa
Marvin <hi@marvr.me>
2025-01-05 08:55:15 +0100

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# jquery-full-year
+
+A jQuery plugin to display the current year, powered by [getfullyear.com](https://getfullyear.com).
+
+## Usage
+
+```js
+$('.year').fullYear();
+```
---
+++ jquery.full-year.js
@@ -0,0 +1,19 @@
+(function($) {
+  $.fn.fullYear = function() {
+    return this.each(function() {
+      const $element = $(this);
+      
+      $.ajax({
+        url: 'https://getfullyear.com/api/year',
+        method: 'GET',
+        success: function(response) {
+          $element.text(response.year);
+        },
+        error: function() {
+          console.error('Error loading year')
+          $element.text('¯\_(ツ)_/¯');
+        }
+      });
+  });
+  };
+})(jQuery);
